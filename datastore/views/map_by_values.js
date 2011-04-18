73189703e32c588044ae365e63a07a2015d0c0d3
diptanu <admin.nitjece@gmail.com>
2011-04-18 19:42:11 +0530

    Minor refactoring in a JS file

--- datastore/views/map_by_values.js
+++ datastore/views/map_by_values.js
@@ -1,17 +1,20 @@
 function(doc) {
-    if (doc.void) { return null; }
-      for (k in doc.data){
-        value = {};
-        var date = new Date(doc.event_time);
-        key = [doc.entity_backing_field.aggregation_paths['_type'],
-           doc.entity_backing_field._id,k,date.getUTCFullYear(), date.getUTCMonth() + 1,
-           date.getUTCDate(), date.getUTCHours(), date.getUTCMinutes(), date.getUTCSeconds()];
-        value["timestamp"] = date.getTime();
-        value["type"] = doc.data[k].type;
-        value["value"] = doc.data[k].value;
-        value["field"] = k;
-        value["entity_id"] = doc.entity_backing_field._id;
-        value["location"] = doc.entity_backing_field.aggregation_paths['_geo'];
-        emit(key, value);
-      }
+  if (!doc.void) {
+    for (k in doc.data){
+      value = {};
+      var date = new Date(doc.event_time);
+      key = [doc.entity_backing_field.aggregation_paths['_type'],
+             doc.entity_backing_field._id,k,date.getUTCFullYear(), date.getUTCMonth() + 1,
+             date.getUTCDate(), date.getUTCHours(), date.getUTCMinutes(), date.getUTCSeconds()];
+      value["timestamp"] = date.getTime();
+      value["type"] = doc.data[k].type;
+      value["value"] = doc.data[k].value;
+      value["field"] = k;
+      value["entity_id"] = doc.entity_backing_field._id;
+      value["location"] = doc.entity_backing_field.aggregation_paths['_geo'];
+      emit(key, value);
+    }
+  }
+  
 }
+

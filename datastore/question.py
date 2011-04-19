33ea81f7e624b7525cd667960e971a6a073a559e
Asif Momin <asifmom@thoughtworks.com>
2011-04-19 17:38:42 +0530

    Added Questions, Question Builder for the Questionnaire model <Akshay,Asif>

--- datastore/question.py
+++ datastore/question.py
@@ -5,15 +5,13 @@ class Question(object):
     NAME = "name"
     LABEL = "label"
     TYPE = "type"
-    SMS_CODE="sms_code"
-    DEFAULT_VALUE="defaultValue"
+    SMS_CODE = "sms_code"
 
     _DEFAULT_VALUES = {
         NAME: "",
         TYPE: "",
-        SMS_CODE:"",
-        DEFAULT_VALUE:""
-        }
+        SMS_CODE: "",
+    }
 
     _DEFAULT_LANGUAGE_SPECIFIC_VALUES = {
         LABEL: [],
@@ -26,11 +24,73 @@ class Question(object):
         for k, default_value in self._DEFAULT_VALUES.items():
             self._dict[k] = kwargs.get(k, default_value)
 
-        for k,default_langauge_specific_value in self._DEFAULT_LANGUAGE_SPECIFIC_VALUES.items():
+        for k, default_langauge_specific_value in self._DEFAULT_LANGUAGE_SPECIFIC_VALUES.items():
             a = kwargs.get('language', self._DEFAULT_LANGUAGE)
-            language_dict={a:kwargs.get(k)}
-            self._dict[k]=[language_dict]
+            language_dict = {a: kwargs.get(k)}
+            self._dict[k] = [language_dict]
 
 
     def to_json(self):
         return self._dict
+
+
+class IntegerQuestion(Question):
+    RANGE = "range"
+    _INTEGER_QUESTION_DEFAULT_VALUES = {
+        RANGE: {}
+    }
+
+
+    def __init__(self, **kwargs):
+        Question.__init__(self,**kwargs)
+        for k, default_value in self._INTEGER_QUESTION_DEFAULT_VALUES.items():
+            self._dict[k]=kwargs.get(k,default_value)
+
+class TextQuestion(Question):
+    DEFAULT_VALUE = "defaultValue"
+    _TEXT_QUESTION_DEFAULT_VALUES = {
+        DEFAULT_VALUE: ""
+    }
+
+
+    def __init__(self, **kwargs):
+        Question.__init__(self,**kwargs)
+        for k, default_value in self._TEXT_QUESTION_DEFAULT_VALUES.items():
+            self._dict[k]=kwargs.get(k,default_value)
+
+
+class QuestionBuilder(object):
+    TYPES = ('integer', 'text')
+
+    TYPES_CLASSES = (IntegerQuestion, TextQuestion)
+
+    TYPES_NAMES_TO_CLASSES = zip(TYPES, TYPES_CLASSES)
+
+    def __new__(cls, *args, **kwargs):
+        """
+            Create the proper QuestionType according to the name passed
+        """
+
+        try:
+            type_name = cls.clean(kwargs.get("type"))
+        except IndexError:
+            raise Exception('You must pass type as '\
+                                 'keyword argument')
+
+        mapping = dict(cls.TYPES_NAMES_TO_CLASSES)
+        return mapping[type_name](**kwargs)
+
+
+    @classmethod
+    def clean(cls, type_name):
+        """
+            Normalize the type name, then check if it is a valid type name.
+        """
+        type_name = type_name.lower()
+        if type_name not in cls.TYPES:
+            raise Exception('"%(type_name)s" is not a valid type name. '\
+                                 'Choose among: "%(allowed_type_names)s"' % {
+                                 'type_name': type_name,
+                                 'allowed_type_names': '", "'.join(cls.TYPES) })
+        return type_name
+
--- datastore/tests/test_question.py
+++ datastore/tests/test_question.py
@@ -1,6 +1,6 @@
 # vim= ai ts=4 sts=4 et sw=4 encoding=utf-8
 import unittest
-from mangrove.datastore.question import Question
+from mangrove.datastore.question import IntegerQuestion, TextQuestion, QuestionBuilder
 
 class TestQuestion(unittest.TestCase):
     def setup(self):
@@ -9,17 +9,41 @@ class TestQuestion(unittest.TestCase):
     def test_should_create_text_question_type_for_default_english_language(self):
         expected_json = {
             "defaultValue": "some default value",
-            "label": [{"eng":"What is your name"}],
+            "label": [{"eng": "What is your name"}],
             "name": "question1_Name",
             "sms_code": "Q1",
             "type": "text"
         }
-        question = Question(name="question1_Name", type="text", sms_code="Q1", label="What is your name",
-                            defaultValue="some default value",language="eng")
+        question = QuestionBuilder(type="text", name="question1_Name", sms_code="Q1", label="What is your name",
+                                defaultValue="some default value", language="eng")
         actual_json = question.to_json()
-        self.assertEqual(actual_json,expected_json)
+        self.assertEqual(actual_json, expected_json)
 
+    def test_should_create_integer_question_type_for_default_english_language(self):
+        expected_json = {
+            "label": [{"eng": "What is your age"}],
+            "name": "Age",
+            "sms_code": "Q2",
+            "range": {},
+            "type": "integer",
+            }
+        question = QuestionBuilder(type="integer",name="Age",  sms_code="Q2", label="What is your age",
+                                   language="eng")
+        actual_json = question.to_json()
+        self.assertEqual(actual_json, expected_json)
 
+    def test_should_create_integer_question_type_for_default_english_language_with_range(self):
+        expected_json = {
+            "label": [{"eng": "What is your age"}],
+            "name": "Age",
+            "sms_code": "Q2",
+            "range": {"min": 15,"max": 120},
+            "type": "integer",
+            }
+        question = QuestionBuilder(name="Age", type="integer", sms_code="Q2", label="What is your age",
+                                   language="eng",range={"min": 15,"max": 120})
+        actual_json = question.to_json()
+        self.assertEqual(actual_json, expected_json)
 
 
     

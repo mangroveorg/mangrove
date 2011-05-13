c65c0c7b2d673cf92c78900db2834139d6841303
Akshay Naval <akshaysn@akshaysn-Latitude-E6500.(none)>
2011-05-13 15:49:34 +0530

    Creating implicit data dict for a question in datawinners

--- datastore/tests/test_data_dict.py
+++ datastore/tests/test_data_dict.py
@@ -3,7 +3,7 @@ from datetime import datetime
 import unittest
 from mock import Mock, patch
 from mangrove.datastore.database import DatabaseManager
-from mangrove.datastore.datadict import DataDictType
+from mangrove.datastore.datadict import DataDictType, create_ddtype
 
 
 class TestDataDict(unittest.TestCase):
@@ -11,12 +11,12 @@ class TestDataDict(unittest.TestCase):
         self.dbm = Mock(spec=DatabaseManager)
         self.patcher = patch('mangrove.datastore.documents.utcnow')
         self.datetime_module = self.patcher.start()
+        self.datetime_module.return_value = datetime(2010,01,01,10,11,12)
 
     def tearDown(self):
         self.patcher.stop()
 
     def test_should_convert_to_json(self):
-        self.datetime_module.return_value = datetime(2010,01,01,10,11,12)
         _id = "1"
         name_type = DataDictType(self.dbm, name='First name', slug='first_Name', primitive_type='string',id = _id)
         expected_json = {'_id': _id,
@@ -34,7 +34,6 @@ class TestDataDict(unittest.TestCase):
 
 
     def test_should_create_from_json(self):
-        self.datetime_module.return_value = datetime(2010,01,01,10,11,12)
         _id = "1"
         first_name = u'First name'
         primitive_type = u'string'
@@ -58,3 +57,18 @@ class TestDataDict(unittest.TestCase):
         self.assertEqual(slug,ddtype.slug)
         self.assertEqual({},ddtype.constraints)
         self.assertEqual([],ddtype.tags)
+
+
+    def test_should_create_ddtype(self):
+        NAME = 'Default Datadict Type'
+        SLUG = 'default'
+        TYPE = 'string'
+        DESC = 'description'
+        ddtype = create_ddtype(self.dbm, name=NAME,
+                                      slug=SLUG, primitive_type=TYPE,constraints={}, description=DESC)
+
+        self.assertEqual(NAME,ddtype.name)
+        self.assertEqual(DESC,ddtype.description)
+        self.assertEqual(SLUG,ddtype.slug)
+        self.assertEqual(TYPE,ddtype.primitive_type)
+        self.dbm.save.assert_called_once_with(ddtype)

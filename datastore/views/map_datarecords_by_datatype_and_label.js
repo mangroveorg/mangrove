a8e9c33a0005dab6da71c3d380278e22d340331f
Mark Johnston <mejymejy@gmail.com>
2011-05-31 12:04:37 -0400

    Removed unused map.

--- datastore/views/map_datarecords_by_datatype_and_label.js
+++
@@ -1,8 +0,0 @@
-function(doc) {
-    if (doc.document_type == 'DataRecord') {
-        for (var label in doc.data) {
-            var id = doc.data[label]['type']['_id'];
-            emit([id, label], null);
-        }
-    }
-}

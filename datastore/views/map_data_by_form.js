05fa04ccd199230bd47f843f05136ca2a5888097
Asif Momin <asifmom@thoughtworks.com>
2011-06-06 17:54:49 +0530

    Changing the name of entity backing field in data_by_form view : Akshay/Asif>

--- datastore/views/map_data_by_form.js
+++ datastore/views/map_data_by_form.js
@@ -1,8 +1,8 @@
 function(doc) {
-    if (!doc.void && doc.document_type == "DataRecord" && doc.form_code != null )
+    if (!doc.void && doc.document_type == "DataRecord" && doc.form_code != null)
     {
         var time = Date.parse(doc.event_time);
-        var short_code = doc.entity_backing_field.short_code;
+        var short_code = doc.entity.short_code;
         var form_code = doc.form_code;
         for (k in doc.data) {
             value = {};

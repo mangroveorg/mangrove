6aae4d7662ff55f1c59aa4e5c708bddb34c6b352
Asif Momin <asifmom@thoughtworks.com>
2011-06-06 17:33:58 +0530

    initial commit for report api -> view data by formcode.<Akshay/Asif>

---
+++ datastore/views/map_data_by_form.js
@@ -0,0 +1,16 @@
+function(doc) {
+    if (!doc.void && doc.document_type == "DataRecord" && doc.form_code != null )
+    {
+        var time = Date.parse(doc.event_time);
+        var short_code = doc.entity_backing_field.short_code;
+        var form_code = doc.form_code;
+        for (k in doc.data) {
+            value = {};
+            value["value"] = doc.data[k].value;
+            value["timestamp"] = time;
+            value["short_code"] = short_code
+            key = [form_code,short_code,k,time]
+            emit(key, value)
+        }
+    }
+}

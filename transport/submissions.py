f44629e97e63d7f881c45ca33eb2f78292245c63
Aroj George <apgeorge@thoughtworks.com>
2011-07-19 17:02:11 +0530

    Refactorings

--- transport/submissions.py
+++ transport/submissions.py
@@ -39,20 +39,20 @@ class SubmissionHandler(object):
     def __init__(self, dbm):
         assert isinstance(dbm, DatabaseManager)
         self.dbm = dbm
+        self.logger = SubmissionLogger(self.dbm)
 
-    def save_data_and_update_log(self, e, form_submission, submission_information, logger, submission_id, is_form_in_test_mode=False):
-        data_record_id = e.add_data(data=form_submission.values, submission=submission_information)
 
-        logger.update_submission_log(submission_id=submission_id, data_record_id=data_record_id, status=True,
-                                     errors=[], in_test_mode=is_form_in_test_mode)
+    def save_data(self, entity, form_submission, submission_id,form):
+        submission_information = dict(submission_id=submission_id, form_code=form.form_code)
+        data_record_id = entity.add_data(data=form_submission.values, submission=submission_information)
         return data_record_id
 
     def _should_accept_submission(self, form):
         return form.is_inactive()
 
-    def _reject_submission_for_inactive_forms(self, form, logger, submission_id):
+    def _reject_submission_for_inactive_forms(self, form, submission_id):
         if self._should_accept_submission(form):
-            logger.update_submission_log(submission_id, False, 'Inactive form_model')
+            self.logger.update_submission_log(submission_id, False, 'Inactive form_model')
             raise InactiveFormModelException(form.form_code)
 
     def accept(self, request):
@@ -60,36 +60,44 @@ class SubmissionHandler(object):
         form_code = request.form_code
         values = request.submission
 
-        logger = SubmissionLogger(self.dbm)
-        submission_id = logger.create_submission_log(request)
-        submission_information = dict(submission_id=submission_id, form_code=form_code)
+        submission_id = self.logger.create_submission_log(request)
 
         form = get_form_model_by_code(self.dbm, form_code)
+        cleaned_data,data_record_id,entity,_status,_errors = self.submit(form,submission_id,values)
 
-        self._reject_submission_for_inactive_forms(form, logger, submission_id)
+        self.logger.update_submission_log(submission_id=submission_id,data_record_id=data_record_id,status=_status,errors=[],in_test_mode=form.is_form_in_test_mode)
+
+        short_code = entity.short_code if form.is_registration_form() else None
+
+        return SubmissionResponse(True, submission_id, {}, data_record_id, short_code=short_code, processed_data=cleaned_data)
+
+    def get_entity(self, form, form_submission):
+        should_create_entity = form.is_registration_form()
+        e = form_submission.to_entity(self.dbm, create=should_create_entity)
+        return e
+
+    def submit(self, form,submission_id,values):
+
+        self._reject_submission_for_inactive_forms(form,submission_id)
 
         if form.entity_defaults_to_reporter():
-            short_code = reporter.get_short_code_from_reporter_number(self.dbm, request.source)
-            values[ENTITY_QUESTION_DISPLAY_CODE]=short_code
+                short_code = reporter.get_short_code_from_reporter_number(self.dbm, "")
+                values[ENTITY_QUESTION_DISPLAY_CODE]=short_code
+
         form_submission = form.validate_submission(values)
+
+        data_record_id = None
+        _errors = []
+        _entity = None
         if form_submission.is_valid:
             if len(form_submission.values) == 1:
                 raise NoQuestionsSubmittedException()
             try:
-                should_create_entity = form.is_registration_form()
-                e = form_submission.to_entity(self.dbm, create=should_create_entity)
-                data_record_id = self.save_data_and_update_log(e, form_submission, submission_information, logger,
-                                                           submission_id, form.is_in_test_mode())
-                short_code = e.short_code if form.is_registration_form() else None
-                return SubmissionResponse(True, submission_id, {}, data_record_id, short_code=short_code, processed_data=form_submission.cleaned_data)
-
-            except DataObjectNotFound as e:
-                logger.update_submission_log(submission_id=submission_id, status=False, errors=e.message, in_test_mode=form.is_in_test_mode())
-                raise DataObjectNotFound('Subject','Unique Identification Number(ID)',form_submission.short_code)
+                entity = self.get_entity(form, form_submission)
+                data_record_id = self.save_data(entity, form_submission,submission_id,form)
         else:
             _errors = form_submission.errors
-            logger.update_submission_log(submission_id=submission_id, status=False, errors=_errors.values(), in_test_mode=form.is_in_test_mode())
-            return SubmissionResponse(False, submission_id, _errors, processed_data=form_submission.cleaned_data)
+
 
 
 class SubmissionLogger(object):

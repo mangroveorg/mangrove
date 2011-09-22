3b6faa54d5c251947c48dcedb9dd98c3cb7fb055
Asif Momin <asifmom@thoughtworks.com>
2011-09-22 14:39:43 +0530

    documentation for pre defined aggregation

--- datastore/time_period_aggregation.py
+++ datastore/time_period_aggregation.py
@@ -3,6 +3,88 @@ from _collections import defaultdict
 from mangrove.form_model.form_model import get_form_model_by_code
 from mangrove.utils.types import is_empty
 
+"""
+    Example usage of predefined aggregate:
+
+    1. Monthly Aggregate on all data records for a field per entity for the form code.
+
+    values = aggregate_for_time_period(
+        self.manager,
+        form_code='CL1',
+        aggregates=[Sum("patients"), Min('meds'), Max('beds'),Latest("director")],
+        period=Month(2, 2010)
+        )
+
+    Returns one row per entity, with the aggregated values for each
+    field.
+    {"<entity_id>": {"patients": 10, 'meds': 20, 'beds': 300 , 'director': "Dr. A"}}
+
+    2. Weekly Aggregate on all data records for a field per entity for the form code.
+
+    values = aggregate_for_time_period(
+        self.manager,
+        form_code='CL1',
+        aggregates=[Sum("patients"), Min('meds'), Max('beds'),Latest("director")],
+        period=Week(52, 2009)
+        )
+
+    52 is the weeknumber and 2009 is the year.
+    Returns one row per entity, with the aggregated values for each field.
+    {"<entity_id>": {"patients": 10, 'meds': 20, 'beds': 300 , 'director': "Dr. A"}}
+
+    2. Yearly Aggregate on all data records for a field per entity for the form code.
+
+    values = aggregate_for_time_period(
+        self.manager,
+        form_code='CL1',
+        aggregates=[Sum("patients"), Min('meds'), Max('beds'),Latest("director")],
+        period=Year(2010)
+        )
+
+    2010 is the year.
+    Returns one row per entity, with the aggregated values for each field.
+    {"<entity_id>": {"patients": 10, 'meds': 20, 'beds': 300 , 'director': "Dr. A"}}
+
+    2. Aggregate on a location level = 2
+
+    #TODO
+    Returns {("India", "MH"): {"patients": 2}}
+
+    3. All entities, selected fields, filtered by location,
+
+    #TODO
+
+    This returns you one row per entity for all entities of type
+    entity_type in Pune with the aggregations applied per field.
+
+    4. Aggregate on a location level = 2, but filter by location,
+
+    #TODO
+
+    5. Aggregate on any hierarchy,
+
+    #TODO
+
+    6. Fetch aggregation for all the fields for all entities of a
+    given type, use '*' instead of field name,
+
+    #TODO
+"""
+
+def aggregate_for_time_period(dbm, form_code, period, aggregates=None, aggregate_on=None, filter=None,
+                              include_grand_totals=False):
+    form_model = get_form_model_by_code(dbm, form_code)
+    statsdict = _get_stats_aggregation(aggregates, dbm, form_model, period)
+
+    if include_grand_totals is True:
+        _calculate_grand_total(statsdict)
+
+    if _latest_aggregation_required(aggregates):
+        latestdict = _get_latest_aggregation(aggregates, dbm, form_model, period)
+        return _merge(statsdict, latestdict)
+
+    return statsdict
+
 
 class Aggregate(object):
     def __init__(self, field_name, aggregate_name):
@@ -173,19 +255,6 @@ def _calculate_grand_total(statsdict):
 
     statsdict[GRAND_TOTALS]=intermediate_dict
 
-def aggregate_for_time_period(dbm, form_code, period, aggregates=None, aggregate_on=None, filter=None,
-                              include_grand_totals=False):
-    form_model = get_form_model_by_code(dbm, form_code)
-    statsdict = _get_stats_aggregation(aggregates, dbm, form_model, period)
-
-    if include_grand_totals is True:
-        _calculate_grand_total(statsdict)
-
-    if _latest_aggregation_required(aggregates):
-        latestdict = _get_latest_aggregation(aggregates, dbm, form_model, period)
-        return _merge(statsdict, latestdict)
-
-    return statsdict
 
 
 def _merge_grand_total(resultdict, statsdict):

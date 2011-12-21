b4560b30f1b08c6d3de8a8a655a58b97187f1350
Neetu Mishra <neetumis@thoughtworks.com>
2011-12-21 16:06:02 +0530

    Added test for Reporter work flow

--- mangrove/transport/tests/test_facade.py
+++ mangrove/transport/tests/test_facade.py
@@ -1,5 +1,5 @@
 import unittest
-from mock import Mock
+from mock import Mock, patch
 from mangrove.datastore.database import DatabaseManager
 from mangrove.datastore.entity import Entity
 from mangrove.form_model.field import TextField
@@ -43,3 +43,45 @@ class TestActivityWorkFlow(unittest.TestCase):
         self.form_model_mock.entity_question = TextField(name="entity question", code="foo", label="bar", ddtype=Mock())
         self.assertEquals({'l':'None'}, activity_report.process({'l':'None'}))
 
+class TestReporterWorkFlow(unittest.TestCase):
+
+    def setUp(self):
+        self.dbm = Mock(spec=DatabaseManager)
+        self.form_model_mock = Mock(spec=FormModel)
+        self.get_entity_count = patch('mangrove.transport.facade.get_entity_count_for_type', new=dummy_get_entity_count_for_type,spec=True)
+        self.get_entity_count.start()
+
+    def tearDown(self):
+        self.get_entity_count.stop()
+
+    def test_should_not_generate_default_code_if_short_code_is_given_and_should_set_geocode_if_location_is_given(self):
+        reporter_work_flow = RegistrationWorkFlow(self.dbm, self.form_model_mock, DummyLocationTree(), get_location_hierarchy=dummy_get_location_hierarchy)
+        self.form_model_mock.get_short_code = Mock(return_value='1')
+        self.assertTrue(reporter_work_flow.process({'l':'Pune'})['g'] == '-12 60')
+        self.assertEquals('1',reporter_work_flow.form_model.get_short_code())
+
+    def test_should_not_generate_default_code_if_short_code_is_given_and_should_set_location_if_geocode_is_given(self):
+        reporter_work_flow = RegistrationWorkFlow(self.dbm, self.form_model_mock, DummyLocationTree(), get_location_hierarchy=dummy_get_location_hierarchy)
+        self.form_model_mock.get_short_code = Mock(return_value='1')
+        self.assertEquals({'l':[u'arantany'],'g':'1 1'}, reporter_work_flow.process({'l':'None','g':'1 1'}))
+        self.assertEquals('1',reporter_work_flow.form_model.get_short_code())
+
+    def test_should_generate_default_code_if_short_code_is_empty_and_should_set_geocode_if_location_is_given(self):
+        reporter_work_flow = RegistrationWorkFlow(self.dbm, self.form_model_mock, DummyLocationTree(), dummy_get_location_hierarchy)
+        self.form_model_mock.get_short_code = Mock(return_value=None)
+        self.form_model_mock.entity_question = TextField(name="entity question", code="s", label="bar", ddtype=Mock())
+        values = reporter_work_flow.process({'t': 'clinic', 'l':'Pune'})
+        self.assertEqual({'s': 'cli1', 't': 'clinic', 'g': '-12 60', 'l': [u'arantany']}, values)
+
+def dummy_get_entity_count_for_type(dbm, entity_type):
+    return 0
+
+def dummy_get_location_hierarchy(foo):
+    return [u'arantany']
+
+class DummyLocationTree(object):
+    def get_location_hierarchy_for_geocode(self, lat, long ):
+        return ['madagascar']
+
+    def get_centroid(self, location_name, level):
+        return 60, -12

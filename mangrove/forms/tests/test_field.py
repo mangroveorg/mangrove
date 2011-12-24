6dcbc8c28e4cda0613626e48449c16b1c00a09be
Diptanu Choudhury <admin.nitjece@gmail.com>
2011-12-24 17:09:51 +0530

    Added the to_json method to TextField

---
+++ mangrove/forms/tests/test_field.py
@@ -0,0 +1,20 @@
+import unittest
+from mangrove.forms.fields import TextField
+
+class TestTestField(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_create_text_field(self):
+        f = TextField("name", "na", "What is the name?")
+        self.assertEqual("na", f.code)
+
+    def test_text_field_to_json(self):
+        f = TextField("name", "na", "What is the name?")
+        self.assertEqual({'name':"name",
+                          'code':"na",
+                          "label":"What is the name?"}, f.to_json())
--- mangrove/forms/tests/test_form_api.py
+++ mangrove/forms/tests/test_form_api.py
@@ -13,3 +13,4 @@ class TestFormAPI(unittest.TestCase):
 
 
 
+

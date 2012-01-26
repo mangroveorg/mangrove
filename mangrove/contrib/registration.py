77bea5b2ba6e7c25d67e1ac7bea6a5aefd01c213
Asif Momin <asifmom@thoughtworks.com>
2012-01-26 16:57:30 +0530

    constant for global registration form code

--- mangrove/contrib/registration.py
+++ mangrove/contrib/registration.py
@@ -5,6 +5,8 @@ from mangrove.form_model.field import HierarchyField, TextField, TelephoneNumber
 from mangrove.form_model.form_model import ENTITY_TYPE_FIELD_NAME, ENTITY_TYPE_FIELD_CODE, NAME_FIELD, NAME_FIELD_CODE, SHORT_CODE, SHORT_CODE_FIELD, LOCATION_TYPE_FIELD_NAME, LOCATION_TYPE_FIELD_CODE, MOBILE_NUMBER_FIELD, MOBILE_NUMBER_FIELD_CODE, DESCRIPTION_FIELD_CODE, GEO_CODE_FIELD, FormModel, GEO_CODE, DESCRIPTION_FIELD, REGISTRATION_FORM_CODE
 from mangrove.form_model.validation import TextLengthConstraint, RegexConstraint
 
+GLOBAL_REGISTRATION_FORM_CODE = "reg"
+
 def create_default_reg_form_model(manager):
     form_model = construct_global_registration_form(manager)
     form_model.save()
@@ -53,7 +55,7 @@ def construct_global_registration_form(manager):
                                      defaultValue="some default value", language="en", ddtype=mobile_number_type,
                                      instruction="Enter the subject's number", constraints=(
             _create_constraints_for_mobile_number()), required=False)
-    form_model = FormModel(manager, name="reg", form_code=REGISTRATION_FORM_CODE, fields=[
+    form_model = FormModel(manager, name=GLOBAL_REGISTRATION_FORM_CODE, form_code=REGISTRATION_FORM_CODE, fields=[
         question1, question2, question3, question4, question5, question6, question7], is_registration_model=True, entity_type=["registration"],
         validators=[MandatoryValidator(), MobileNumberValidationsForReporterRegistrationValidator(),
                     AtLeastOneLocationFieldMustBeAnsweredValidator()])

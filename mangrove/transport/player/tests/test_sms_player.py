6f3b69ee2c23bad534f9a5cf670f48b672f74458
akshaysn <akshay.naval@gmail.com>
2012-02-29 17:03:12 +0530

    Fixing the build : Akshay

--- mangrove/transport/player/tests/test_sms_player.py
+++ mangrove/transport/player/tests/test_sms_player.py
@@ -123,7 +123,7 @@ class TestSMSPlayer(TestCase):
             get_form_submission_mock.return_value = self.form_submission_mock
             response = self.sms_player.accept(Request(message=self.message, transportInfo=self.transport))
             values = OrderedDict([(u'id', u'1'), (u'm', u'hello world'), ('l', None)])
-            self.form_model_mock.is_valid.assert_called_once_with(values)
+            self.form_model_mock.is_valid.assert_called_once_with(values=values)
             self.form_submission_mock.save.assert_called_once_with(self.dbm)
             self.assertEqual('', response.datarecord_id)
             self.assertEqual([''], response.entity_type)
@@ -170,7 +170,7 @@ class TestSMSPlayer(TestCase):
         with patch.object(FormSubmissionFactory, 'get_form_submission') as get_form_submission_mock:
             get_form_submission_mock.return_value = self.form_submission_mock
             response = SMSPlayer(self.dbm, self.loc_tree, order_sms_parser).accept(request)
-            self.form_model_mock.is_valid.assert_called_once_with(values)
+            self.form_model_mock.is_valid.assert_called_once_with(values=values)
             self.form_submission_mock.save.assert_called_once_with(self.dbm)
             self.assertEqual('', response.datarecord_id)
             self.assertEqual([''], response.entity_type)

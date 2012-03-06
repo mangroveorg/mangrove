6ba3218c9fcab9152eea93808897bb76b9f69044
akshaysn <akshay.naval@gmail.com>
2012-03-06 12:43:16 +0530

    Akshay | logging after successful and unsuccessful deletion of an entity

--- mangrove/transport/player/handler.py
+++ mangrove/transport/player/handler.py
@@ -1,16 +1,22 @@
 # vim: ai ts=4 sts=4 et sw=4 encoding=utf-8
+from mangrove.errors.MangroveException import DataObjectNotFound
 from mangrove.datastore.entity import invalidate_entity
 from mangrove.transport.player.parser import DeleteRequestParser
 from mangrove.transport.submissions import Submission
 
 class DeleteHandler(object):
-    def __init__(self, dbm):
-        self.parser = DeleteRequestParser()
+    def __init__(self, dbm, parser=None):
+        self.parser = parser or DeleteRequestParser()
         self.dbm = dbm
 
     def handle(self, request):
         command, values = self.parser.parse(request.message)
         submission = Submission(self.dbm, request.transport, command, values)
         submission.save()
-        invalidate_entity(self.dbm, values['entity_type'], values['entity_id'])
+        try:
+            invalidate_entity(self.dbm, values['entity_type'], values['entity_id'])
+            submission.update(True,'')
+        except DataObjectNotFound as exception:
+            submission.update(False, exception.message)
+            raise
 
--- mangrove/transport/player/tests/test_delete_handler.py
+++ mangrove/transport/player/tests/test_delete_handler.py
@@ -1,6 +1,7 @@
 # vim: ai ts=4 sts=4 et sw=4 encoding=utf-8
 from unittest import TestCase
 from mock import patch, Mock, self
+from mangrove.errors.MangroveException import DataObjectNotFound
 from mangrove.datastore.database import DatabaseManager
 from mangrove.transport.player.handler import DeleteHandler
 from mangrove.transport.player.parser import DeleteRequestParser
@@ -28,7 +29,6 @@ class TestDeleteHandler(TestCase):
         request = Request(message=message, transportInfo=transportInfo)
         mock_submission = Mock()
         self.submission_mock.return_value = mock_submission
-
         with patch.object(DeleteRequestParser, 'parse') as parse_mock:
             parse_mock.return_value = command,values
             self.delete_handler.handle(request)
@@ -36,3 +36,36 @@ class TestDeleteHandler(TestCase):
         self.submission_mock.assert_called_once_with(self.manager, transportInfo, command, values)
         mock_submission.save.assert_called_once_with()
         self.invalidate_entity_mock.assert_called_once_with(self.manager, 'entity_type', 'entity_id')
+
+    def test_should_log_when_delete_unsuccessful(self):
+        message = 'delete entity_type entity_id'
+        command = 'delete'
+        values = {'entity_type': 'entity_type', 'entity_id': 'entity_id'}
+        transportInfo = Mock()
+        request = Request(message=message, transportInfo=transportInfo)
+        mock_submission = Mock()
+        self.submission_mock.return_value = mock_submission
+        with patch.object(DeleteRequestParser, 'parse') as parse_mock:
+            parse_mock.return_value = command,values
+            self.delete_handler.handle(request)
+        mock_submission.update.assert_called_once_with(True,'',)
+
+    def test_should_log_when_delete_successful(self):
+        message = 'delete entity_type entity_id'
+        command = 'delete'
+        values = {'entity_type': 'entity_type', 'entity_id': 'entity_id'}
+        transportInfo = Mock()
+        request = Request(message=message, transportInfo=transportInfo)
+        mock_submission = Mock()
+        self.submission_mock.return_value = mock_submission
+        exception = DataObjectNotFound("Entity", "Unique Identification Number (ID)", "entity_id")
+        self.invalidate_entity_mock.side_effect = exception
+        with patch.object(DeleteRequestParser, 'parse') as parse_mock:
+            with self.assertRaises(DataObjectNotFound):
+                parse_mock.return_value = command,values
+                self.delete_handler.handle(request)
+                mock_submission.update.assert_called_once_with(False,exception.message)
+
+
+
+

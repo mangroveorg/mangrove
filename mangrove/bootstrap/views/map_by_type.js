9f276333859aa2073c32a8ae0a37c4f9ca7f0c4e
akshaysn <akshay.naval@gmail.com>
2012-03-29 17:26:08 +0530

    akshay/geet : Adding void check in 'by_type' couch view

--- mangrove/bootstrap/views/map_by_type.js
+++ mangrove/bootstrap/views/map_by_type.js
@@ -1,5 +1,5 @@
 function(doc) {
-    if (doc.document_type == 'Entity') {
+    if (doc.document_type == 'Entity' && !doc.void) {
         for (index in doc.aggregation_paths._type) {
             emit(doc.aggregation_paths._type[index], 1);
         }

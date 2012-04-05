6bc8ffdec7c2888e78b7befa71a334212df4596c
herihaja Rabe <herihaja@hni.org>
2012-04-05 17:35:53 +0300

    Avoiding to register a DS phone number already registered <herihaja>

--- mangrove/transport/player/tests/test_sms_player.py
+++ mangrove/transport/player/tests/test_sms_player.py
@@ -124,7 +124,7 @@ class TestSMSPlayer(TestCase):
             get_form_submission_mock.return_value = self.form_submission_mock
             response = self.sms_player.accept(Request(message=self.message, transportInfo=self.transport))
             values = OrderedDict([(u'id', u'1'), (u'm', u'hello world')])
-            self.form_model_mock.validate_submission.assert_called_once_with(values=values)
+            self.form_model_mock.validate_submission.assert_called_once_with(values=values, registered_phone_numbers=[])
             self.form_submission_mock.save.assert_called_once_with(self.dbm)
             self.assertEqual('', response.datarecord_id)
             self.assertEqual([''], response.entity_type)
@@ -172,7 +172,7 @@ class TestSMSPlayer(TestCase):
         with patch.object(FormSubmissionFactory, 'get_form_submission') as get_form_submission_mock:
             get_form_submission_mock.return_value = self.form_submission_mock
             response = SMSPlayer(self.dbm, self.loc_tree, order_sms_parser).accept(request)
-            self.form_model_mock.validate_submission.assert_called_once_with(values=values)
+            self.form_model_mock.validate_submission.assert_called_once_with(values=values, registered_phone_numbers=[])
             self.form_submission_mock.save.assert_called_once_with(self.dbm)
             self.assertEqual('', response.datarecord_id)
             self.assertEqual([''], response.entity_type)

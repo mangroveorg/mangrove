3219ba1ed253018fb560978dc346597465229d94
Geet Chandratre <geetchandratre@gmail.com>
2012-05-03 15:09:44 +0530

    Geet/Ghante | Adding geocode field and refactoring tests

--- mangrove/transport/xforms/tests/test_xform.py
+++ mangrove/transport/xforms/tests/test_xform.py
@@ -2,12 +2,18 @@ import unittest
 from mock import patch, Mock
 from mangrove.transport.xforms.tests.form_content import expected_response_for_get_all_forms, expected_response_for_get_specific_form
 from mangrove.transport.xforms.xform import list_all_forms, xform_for
+from lxml.doctestcompare import LXMLOutputChecker
 
 class TestXform(unittest.TestCase):
+
+    def setUp(self):
+        self.checker = LXMLOutputChecker()
+
     def test_should_return_list_of_required_forms(self):
         form_tuples = [("name", "id"), ("name2", "id2")]
         base_url = "baseURL"
-        self.assertEquals(list_all_forms(form_tuples, base_url), unicode(expected_response_for_get_all_forms))
+        self.assertTrue(self.checker.check_output(list_all_forms(form_tuples, base_url), unicode(expected_response_for_get_all_forms), 0))
+
 
     def test_should_return_specific_form(self):
         dbm = Mock()
@@ -23,6 +29,6 @@ class TestXform(unittest.TestCase):
         questionnaire_mock.form_code = 'form_code'
         with patch("mangrove.transport.xforms.xform.FormModel") as form_model_mock:
             form_model_mock.get.return_value = questionnaire_mock
-            self.assertEquals(xform_for(dbm, "someFormId"), unicode(expected_response_for_get_specific_form))
+            self.assertTrue(self.checker.check_output(xform_for(dbm, "someFormId"), unicode(expected_response_for_get_specific_form), 0))
 
 
--- mangrove/transport/xforms/xform.py
+++ mangrove/transport/xforms/xform.py
@@ -2,24 +2,30 @@ from jinja2 import Environment, PackageLoader
 from mangrove.form_model.field import field_attributes
 from mangrove.form_model.form_model import FormModel
 
-env = Environment(loader=PackageLoader('mangrove.transport.xforms'),trim_blocks=True)
+env = Environment(loader=PackageLoader('mangrove.transport.xforms'), trim_blocks=True)
 field_xmls = {
-    field_attributes.TEXT_FIELD : env.get_template('text_field.xml'),
-    field_attributes.TELEPHONE_NUMBER_FIELD : env.get_template('text_field.xml'),
-    field_attributes.INTEGER_FIELD : env.get_template('text_field.xml'),
-    field_attributes.DATE_FIELD : env.get_template('text_field.xml'),
-    field_attributes.LIST_FIELD : env.get_template('text_field.xml'),
-    field_attributes.SELECT_FIELD : env.get_template('select_field.xml'),
-    field_attributes.MULTISELECT_FIELD : env.get_template('select_field.xml'),
-    field_attributes.LOCATION_FIELD : env.get_template('text_field.xml'),
-}
+    field_attributes.TEXT_FIELD: env.get_template('text_field.xml'),
+    field_attributes.TELEPHONE_NUMBER_FIELD: env.get_template('text_field.xml'),
+    field_attributes.INTEGER_FIELD: env.get_template('text_field.xml'),
+    field_attributes.DATE_FIELD: env.get_template('text_field.xml'),
+    field_attributes.LIST_FIELD: env.get_template('text_field.xml'),
+    field_attributes.SELECT_FIELD: env.get_template('select_field.xml'),
+    field_attributes.MULTISELECT_FIELD: env.get_template('select_field.xml'),
+    field_attributes.LOCATION_FIELD: env.get_template('text_field.xml'),
+    }
+
+field_types = {
+    field_attributes.LOCATION_FIELD: 'geopoint',
+    field_attributes.TEXT_FIELD: 'string',
+    }
 
 def list_all_forms(form_tuples, xform_base_url):
     template = env.get_template('form_list.xml')
     return template.render(form_tuples=form_tuples, xform_base_url=xform_base_url)
 
-def xform_for(dbm, form_id) :
+
+def xform_for(dbm, form_id):
     questionnaire = FormModel.get(dbm, form_id)
     template = env.get_template('form.xml')
-    return template.render(questionnaire=questionnaire, field_xmls=field_xmls)
+    return template.render(questionnaire=questionnaire, field_xmls=field_xmls, field_types=field_types)
 

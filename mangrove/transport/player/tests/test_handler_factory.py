792220e373ab35b7fa33024cc35146100063df4a
Argha <argha@argha-VirtualBox.(none)>
2012-06-12 17:28:39 +0530

    Argha - #801 - changes to handler tests to reflect new code changes.

--- mangrove/transport/player/tests/test_handler_factory.py
+++ mangrove/transport/player/tests/test_handler_factory.py
@@ -7,11 +7,14 @@ from mangrove.transport.player.handler import handler_factory, DeleteHandler, Su
 class TestHandlerFactory(TestCase):
     def setUp(self):
         self.dbm = Mock()
+        self.form_model = Mock()
+        self.form_model.form_code = ENTITY_DELETION_FORM_CODE
 
     def test_should_get_delete_handler_for_delete_form_model(self):
-        self.assertIsInstance(handler_factory(self.dbm, ENTITY_DELETION_FORM_CODE), DeleteHandler)
+        self.assertIsInstance(handler_factory(self.dbm, self.form_model), DeleteHandler)
 
     def test_should_get_submission_handler_for_all_other_form_models(self):
-        self.assertIsInstance(handler_factory(self.dbm, 'test_form_model'), SubmissionHandler)
+        self.form_model.form_code = None
+        self.assertIsInstance(handler_factory(self.dbm, self.form_model), SubmissionHandler)
 
 

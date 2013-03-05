d7e29ace24e29a95e531aa5ef1e07db0c188a08c
lakshmirp <rp.lakshmi.45@gmail.com>
2013-03-05 15:14:32 +0530

    anishek/ashwin added ability to report the date in submitted format for keyword search

--- mangrove/form_model/field.py
+++ mangrove/form_model/field.py
@@ -329,14 +329,22 @@ class DateField(Field):
             raise IncorrectDate(self._dict.get(field_attributes.FIELD_CODE), date_string,
                 self._dict.get(self.DATE_FORMAT))
 
-
+#All the Field Types should be be wrapped with Excel Field types defined in other project including the lead part fields.
+#That will require atleast a couple of days of work
 class ExcelDate(object):
     EXCEL_DATE_DICTIONARY = {'mm.yyyy': 'MMM, YYYY', 'dd.mm.yyyy': 'MMM DD, YYYY', 'mm.dd.yyyy': 'MMM DD, YYYY',
                              'submission_date': 'MMM DD, YYYY hh:mm:ss'}
+    DATE_DICTIONARY = {'mm.yyyy': '%m.%Y', 'dd.mm.yyyy': '%d.%m.%Y', 'mm.dd.yyyy': '%m.%d.%Y'}
 
     def __init__(self, date, date_format):
         self.date = date
+        self.date_format = date_format
         self.excel_cell_date_format = ExcelDate.EXCEL_DATE_DICTIONARY.get(date_format)
+
+
+    def date_as_string(self):
+        '''This is implemented for KeywordFilter.filter method. Ideally we should pass in the keyword here and return true or false.'''
+        return self.date.strftime(ExcelDate.DATE_DICTIONARY.get(self.date_format))
 
     def __eq__(self, other):
         return self.date == other.date and self.excel_cell_date_format == other.excel_cell_date_format

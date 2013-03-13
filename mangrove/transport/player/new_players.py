a5abeb55b38733ae0f737ee6c963716707bd3d9a
lakshmirp <rp.lakshmi.45@gmail.com>
2013-03-13 15:48:48 +0530

    anishek/shruthi - adding the Version 2 players file

---
+++ mangrove/transport/player/new_players.py
@@ -0,0 +1,53 @@
+import inspect
+from mangrove.form_model.form_model import NAME_FIELD
+from mangrove.transport.player.parser import WebParser, SMSParserFactory
+from mangrove.transport.services.survey_response_service import SurveyResponseService
+from mangrove.transport import reporter
+
+class WebPlayerV2(object):
+    def __init__(self, dbm):
+        self.dbm = dbm
+
+    def add_survey_response(self, request, logger=None):
+        assert request is not None
+        form_code, values = self._parse(request.message)
+        service = SurveyResponseService(self.dbm, logger)
+        return service.save_survey(form_code, values, [], request.transport, request.message)
+
+    def _parse(self, message):
+        return WebParser().parse(message)
+
+
+class SMSPlayerV2(object):
+    def __init__(self, dbm, post_sms_parser_processors):
+        self.post_sms_parser_processor = post_sms_parser_processors if post_sms_parser_processors else []
+        self.dbm = dbm
+
+    def _post_parse_processor(self, form_code, values, extra_elements=[]):
+        for post_sms_parser_processors in self.post_sms_parser_processor:
+            if len(inspect.getargspec(post_sms_parser_processors.process)[0]) == 4:
+                response = post_sms_parser_processors.process(form_code, values, extra_elements)
+            else:
+                response = post_sms_parser_processors.process(form_code, values)
+            if response is not None:
+                return response
+
+    def add_survey_response(self, request, logger=None):
+        form_code, values, extra_elements = self._parse(request.message)
+        post_sms_processor_response = self._post_parse_processor(form_code, values, extra_elements)
+
+        if post_sms_processor_response is not None:
+            if logger is not None:
+                log_entry = "message:message " + repr(request.message) + "|source: " + request.transport.source + "|"
+                log_entry += "Status: False"
+                logger.info(log_entry)
+            return post_sms_processor_response
+
+        reporter_entity = reporter.find_reporter_entity(self.dbm, request.transport.source)
+        reporter_entity_names = [{NAME_FIELD: reporter_entity.value(NAME_FIELD)}]
+
+        service = SurveyResponseService(self.dbm, logger)
+        return service.save_survey(form_code, values, reporter_entity_names, request.transport, request.message)
+
+    def _parse(self, message):
+        return SMSParserFactory().getSMSParser(message, self.dbm).parse(message)

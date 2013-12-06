79430f694525e5cd9f31e81254fa98b864af4b01
PoojaS <poojas2406@gmail.com>
2013-12-06 10:25:25 +0530

    Pooja/DC | #0000 | Removing unused views

--- mangrove/bootstrap/views/map_survey_response_by_datasender.js
+++
@@ -1,8 +0,0 @@
-function(doc) {
-    var isNotNull = function(o) {
-        return !((o === undefined) || (o == null));
-    };
-    if (doc.document_type == 'SurveyResponse' && isNotNull(doc.form_code)) {
-        emit(doc.owner_uid, null);
-    }
-}

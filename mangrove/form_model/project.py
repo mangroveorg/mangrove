e95722975334c9718258b967a21361c7fba4ca34
herihaja <hery.imiary@gmail.com>
2015-02-11 05:04:19 +0300

    Heri | #4161 | Excel upgrade for xlsform (part 2)

--- mangrove/form_model/project.py
+++ mangrove/form_model/project.py
@@ -200,15 +200,20 @@ class Project(FormModel):
 
     def has_attachment(self):
         try: # find a better way to check attachement exisits
-            attachement = self.get_attachments('questionnaire.xls')
+            attachment = self.get_attachments('questionnaire.xls')
             return True, attachment, '.xls'
         except LookupError:
             try:
-                attachement = self.get_attachments('questionnaire.xlsx')
-                return True, attachement, '.xlsx'
+                attachment = self.get_attachments('questionnaire.xlsx')
+                return True, attachment, '.xlsx'
             except LookupError:
                 return False, None, None
 
+
+    def update_attachments(self, attachments, attachment_name):
+        extension = self.has_attachment()[2]
+        self.delete_attachment(self._doc, "questionnaire%s" % extension)
+        self.add_attachments(attachments, attachment_name)
     
 
 

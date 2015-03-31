bc53ad35fff5c9b6602d2c57970118ec7fbc8950
DeepthiChand <deepthia@thoughtworks.com>
2015-03-31 15:43:14 +0530

    DC/Venu | #4403 | conditionally removing the group from contact

--- mangrove/datastore/documents.py
+++ mangrove/datastore/documents.py
@@ -177,7 +177,8 @@ class ContactDocument(DocumentBase):
             self.custom_groups.append(group_name)
 
     def remove_custom_group(self, group_name):
-        self.custom_groups.remove(group_name)
+        if group_name in self.custom_groups:
+            self.custom_groups.remove(group_name)
 
 
 class DataRecordDocument(DocumentBase):

f4e7f2aec9b749775c31611a7558f6300b275339
Ajay Divakaran <ajay@dwdev.ajay>
2015-04-13 11:42:01 +0530

    Venu/Ajay/Melanie/DC | #4454 | Bugfix - Add contact to multiple groups not working

--- mangrove/datastore/documents.py
+++ mangrove/datastore/documents.py
@@ -179,8 +179,9 @@ class ContactDocument(DocumentBase):
         self.groups.append(group_name)
 
     def add_custom_group(self, group_name):
-        if not hasattr(self._data, "custom_groups"):
-            self.custom_groups = []
+        if not 'custom_groups' in self._data:
+            self._data['custom_groups'] = []
+
         if group_name not in self.custom_groups:
             self.custom_groups.append(group_name)
 

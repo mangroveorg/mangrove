55bc96892ac5c6598c25c53d0dccae584bd8d479
dwdev <dwdev@dwdev.(none)>
2015-05-27 12:27:53 +0530

    Melanie|#4599| Fixing UT

--- mangrove/transport/player/tests/test_order_sms_parser.py
+++ mangrove/transport/player/tests/test_order_sms_parser.py
@@ -5,7 +5,6 @@ from mangrove.datastore.database import DatabaseManager
 from mangrove.form_model.form_model import FormModel
 from mangrove.errors.MangroveException import SMSParserInvalidFormatException, SMSParserWrongNumberOfAnswersException
 from mangrove.transport.player.parser import OrderSMSParser, SMSParser
-@SkipTest
 class TestOrderSMSParser(unittest.TestCase):
     def setUp(self):
         self.dbm = Mock(spec=DatabaseManager)
@@ -22,22 +21,24 @@ class TestOrderSMSParser(unittest.TestCase):
     def test_should_return_all_answers(self):
         message = "questionnaire_code q1_answer q2_answer q3_answer"
         self._mock_get_question_codes_from_couchdb(['q1', 'q2', 'q3'])
+        with patch("mangrove.transport.player.parser.SMSParser.get_form_code_and_tokens") as mock_get_form_code_and_tokens:
+            mock_get_form_code_and_tokens.return_value = 'questionnaire_code', ['q1_answer', 'q2_answer', 'q3_answer']
+            values = self.sms_parser.parse(message)
+            question_code_and_answers = {"q1": "q1_answer", "q2": "q2_answer", "q3": "q3_answer"}
+            expected = ("questionnaire_code", question_code_and_answers, [])
 
-        values = self.sms_parser.parse(message)
-        question_code_and_answers = {"q1": "q1_answer", "q2": "q2_answer", "q3": "q3_answer"}
-        expected = ("questionnaire_code", question_code_and_answers, [])
-
-        self.assertEqual(expected, values)
-        self.get_question_codes_patch.stop()
+            self.assertEqual(expected, values)
+            self.get_question_codes_patch.stop()
 
     def test_should_return_extra_data_with_answers(self):
         message = "questionnaire_code q1_answer q2_answer q3_answer extra1 extra2 extra3"
         self._mock_get_question_codes_from_couchdb(['q1', 'q2', 'q3'])
-
-        values = self.sms_parser.parse(message)
-        question_code_and_answers = {"q1": "q1_answer", "q2": "q2_answer", "q3": "q3_answer"}
-        extra_data = ["extra1", "extra2", "extra3"]
-        expected = ("questionnaire_code", question_code_and_answers, extra_data)
+        with patch("mangrove.transport.player.parser.SMSParser.get_form_code_and_tokens") as mock_get_form_code_and_tokens:
+            mock_get_form_code_and_tokens.return_value = 'questionnaire_code', ['q1_answer', 'q2_answer', 'q3_answer', "extra1", "extra2", "extra3"]
+            values = self.sms_parser.parse(message)
+            question_code_and_answers = {"q1": "q1_answer", "q2": "q2_answer", "q3": "q3_answer"}
+            extra_data = ["extra1", "extra2", "extra3"]
+            expected = ("questionnaire_code", question_code_and_answers, extra_data)
 
         self.assertEqual(expected, values)
         self.get_question_codes_patch.stop()
@@ -51,11 +52,12 @@ class TestOrderSMSParser(unittest.TestCase):
     def test_should_ignore_additional_space_separators(self):
         message = "questionnaire_code q1_answer q2_answer                   q3_answer"
         self._mock_get_question_codes_from_couchdb(['q1', 'q2', 'q3'])
+        with patch("mangrove.transport.player.parser.SMSParser.get_form_code_and_tokens") as mock_get_form_code_and_tokens:
+            mock_get_form_code_and_tokens.return_value = 'questionnaire_code', ['q1_answer', 'q2_answer', 'q3_answer']
+            values = self.sms_parser.parse(message)
+            question_code_and_answers = {"q1": "q1_answer", "q2": "q2_answer", "q3": "q3_answer"}
+            expected = ("questionnaire_code", question_code_and_answers, [])
 
-        values = self.sms_parser.parse(message)
-        question_code_and_answers = {"q1": "q1_answer", "q2": "q2_answer", "q3": "q3_answer"}
-        expected = ("questionnaire_code", question_code_and_answers, [])
-
-        self.assertEqual(expected, values)
-        self.get_question_codes_patch.stop()
+            self.assertEqual(expected, values)
+            self.get_question_codes_patch.stop()
 

96d24720e2f1f4d5ec73d750a3b33f2335d7d2e0
natch <natchup@sahajsoft.com>
2015-08-11 14:21:04 +0530

    Natchu/Kumaran | #4576,#4757 | Added set_project_ids method for User Permissions Document

--- mangrove/datastore/user_permission.py
+++ mangrove/datastore/user_permission.py
@@ -8,28 +8,31 @@ def get_user_permission(user_id, dbm):
         return None
     return UserPermission.new_from_doc(dbm, UserPermissionDocument.wrap(rows[0]['doc']))
 
+
 def get_questionnaires_for_user(user_id, dbm):
     rows = dbm.load_all_rows_in_view('all_questionnaire_by_user_permission',
                                      key=user_id, include_docs=True)
     questionnaires = [row['doc'] for row in rows]
     return questionnaires
 
+
 class UserPermission(DataObject):
     __document_class__ = UserPermissionDocument
-    
+
     def __init__(self, dbm, user_id=None, project_ids=None):
         super(UserPermission, self).__init__(dbm)
         doc = UserPermissionDocument()
         doc.user_id = user_id
         doc.project_ids = project_ids
         DataObject._set_document(self, doc)
-    
+
     @property
     def user_id(self):
         return self._doc.user_id
-    
+
     @property
     def project_ids(self):
-        return self._doc.project_ids    
-    
-        
+        return self._doc.project_ids
+
+    def set_project_ids(self, project_ids=None):
+        self._doc.project_ids = project_ids

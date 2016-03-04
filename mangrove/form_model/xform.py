686f04ef6d307826761ab574e60d37d2a47ad922
Ramanathan Balakrishnan <sreeramanathan@gmail.com>
2016-03-04 12:23:04 +0530

    Ram/Raj | #4933 | fixing repeat, refactoring

--- mangrove/form_model/xform.py
+++ mangrove/form_model/xform.py
@@ -6,18 +6,31 @@ class Xform(object):
         ET.register_namespace('', 'http://www.w3.org/2002/xforms')
         self.root_node = ET.fromstring(xform)
 
-    def get_node(self, node, field_code):
-        for child in node:
-            if 'ref' in child.attrib and child.attrib['ref'].endswith(field_code):
-                return child
-
     def get_body_node(self):
         return self.root_node._children[1]
 
     def equals(self, another_xform):
         another_xform_str = ET.tostring(another_xform.root_node)\
-            .replace(self._instance_node(another_xform.root_node), self._instance_node(self.root_node))
+            .replace(_instance_node(another_xform.root_node), _instance_node(self.root_node))
         return ET.tostring(self.root_node) == another_xform_str
 
-    def _instance_node(self, node):
-        return node._children[0]._children[1]._children[00]._children[0].attrib['id']
+
+def get_node(node, field_code):
+    for child in node:
+        if child.tag.endswith('repeat'):
+            for inner_child in child:
+                if 'ref' in inner_child.attrib and inner_child.attrib['ref'].endswith(field_code):
+                    return inner_child
+
+        if 'ref' in child.attrib and child.attrib['ref'].endswith(field_code):
+            return child
+
+
+def _instance_node(node):
+    return _child_node(_child_node(_child_node(node, 'head'), 'model'), 'instance')._children[0].attrib['id']
+
+
+def _child_node(node, tag):
+    for child in node:
+        if child.tag.endswith(tag):
+            return child
